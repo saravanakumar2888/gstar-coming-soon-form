ae5a00eb14c40b5072cb91c45cc75e8e031de9e3
Saravana Kumar <saravana.kumar@aceturtle.com>
2025-11-10 18:50:37 +0530

    Gstar Coming Soon Page

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { renderGstarForm, sendGstarEnquiryMail } = require("./gstar");
 
-router.get("/gstar", renderGstarForm);
+router.get("/", renderGstarForm);
 router.post("/gstar-enquiry", sendGstarEnquiryMail);
 
 module.exports = router;
